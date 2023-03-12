365de3718335363cbaf59cb0a2e5657f7c10e180
daniadib <danodeeb77@gmail.com>
2023-03-11 21:46:04 -0500

    second commit

--- src/test/java/tests/HomePageTests.java
+++ src/test/java/tests/HomePageTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.Driver;
@@ -14,6 +15,21 @@ public class HomePageTests extends TestBase{
         String pageTitle = "Delta Air Lines | Flights & Plane Tickets + Hotels & Rental Cars";
 
         Assert.assertEquals(Driver.getDriver().getTitle(), pageTitle);
+
+    }
+
+    @Test
+    public void navigationBar(){
+
+        String navBook = "BOOK";
+        String navCHECKIn = "CHECK-IN";
+        String navMyTRIPS = "MY TRIPS";
+        String navFlightSTATUS = "FLIGHT STATUS";
+
+        Assert.assertEquals(Driver.getDriver().findElement( By.id("headPrimary1")).getText(), navBook);
+        Assert.assertEquals(Driver.getDriver().findElement( By.id("headPrimary2")).getText(), navCHECKIn);
+        Assert.assertEquals(Driver.getDriver().findElement( By.id("headPrimary3")).getText(), navMyTRIPS);
+        Assert.assertEquals(Driver.getDriver().findElement( By.id("headPrimary4")).getText(), navFlightSTATUS);
 
     }
 }
