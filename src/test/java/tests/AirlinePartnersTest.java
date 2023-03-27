9501e4bd90a45f8ddfa7646efb930f7d74742320
Nino Todua <ntodua775@gmail.com>
2023-03-26 22:07:05 -0400

    minor changes were made to Derya's test cases

--- src/test/java/tests/AirlinePartnersTest.java
+++ src/test/java/tests/AirlinePartnersTest.java
@@ -8,75 +8,68 @@ import pages.Flights2Page;
 public class AirlinePartnersTest  extends TestBase {
 
 
-    @Test (priority= 1 , groups = {"smoke"})
+    @Test
     public void testAirlinePartnersMenu() {
         AirlinePartnersPage airlinePartnersPage = new AirlinePartnersPage();
         airlinePartnersPage.airlinePartners();
     }
 
+    @Test
     public void testAirlinePartnersDropDownMenu() {
         AirlinePartnersPage airlinePartnersPage = new AirlinePartnersPage();
-        airlinePartnersPage.clickAirPartnersDropDownMenu();
+        airlinePartnersPage.airlinePartners();
     }
-
+    @Test
     public void testAirFrance() {
         AirlinePartnersPage airlinePartnersPage = new AirlinePartnersPage();
-        airlinePartnersPage.getAirPartnersDropDown().click();
         airlinePartnersPage.selectAirFrance();
-        Assert.assertEquals(airlinePartnersPage.getAirPartnersDropDown().getText(), "Air France");
+        Assert.assertEquals(airlinePartnersPage.getAirFrancePageTitle().getText(), "Air France");
 
     }
-
+    @Test
     public void testAeroMexico() {
         AirlinePartnersPage airlinePartnersPage = new AirlinePartnersPage();
-        airlinePartnersPage.getAirPartnersDropDown().click();
         airlinePartnersPage.selectAeromexico();
-        Assert.assertEquals(airlinePartnersPage.getAirPartnersDropDown().getText(), "AeroMexico");
+        Assert.assertEquals(airlinePartnersPage.getAeroMexicoPageTitle().getText(), "Aeromexico");
     }
-
+    @Test
     public void testChinaEastern() {
         AirlinePartnersPage airlinePartnersPage = new AirlinePartnersPage();
-        airlinePartnersPage.getAirPartnersDropDown().click();
         airlinePartnersPage.selectChinaEastern();
-        Assert.assertEquals(airlinePartnersPage.getAirPartnersDropDown().getText(), "China Eastern");
+        Assert.assertEquals(airlinePartnersPage.getChinaEaternPageTitle().getText(), "China Eastern Airlines");
 
     }
-
+    @Test
     public void testKlm() {
         AirlinePartnersPage airlinePartnersPage = new AirlinePartnersPage();
-        airlinePartnersPage.getAirPartnersDropDown().click();
         airlinePartnersPage.selectKLM();
-        Assert.assertEquals(airlinePartnersPage.getAirPartnersDropDown().getText(), "KLM");
+        Assert.assertEquals(airlinePartnersPage.getKlmPageTitle().getText(), "KLM");
 
     }
-
+    @Test
     public void testKoreanAir() {
         AirlinePartnersPage airlinePartnersPage = new AirlinePartnersPage();
-        airlinePartnersPage.getAirPartnersDropDown().click();
         airlinePartnersPage.selectKoreanAir();
-        Assert.assertEquals(airlinePartnersPage.getAirPartnersDropDown().getText(), "Korean Air");
+        Assert.assertEquals(airlinePartnersPage.getKoreanAirPageTitle().getText(), "Korean Air");
     }
-
+    @Test
     public void testLatam() {
         AirlinePartnersPage airlinePartnersPage = new AirlinePartnersPage();
-        airlinePartnersPage.getAirPartnersDropDown().click();
         airlinePartnersPage.selectLatam();
-        Assert.assertEquals(airlinePartnersPage.getAirPartnersDropDown().getText(), "LATAM");
+        Assert.assertEquals(airlinePartnersPage.getLatamPageTitle().getText(), "LATAM");
 
     }
-
+    @Test
     public void testVirginAtlantic() {
         AirlinePartnersPage airlinePartnersPage = new AirlinePartnersPage();
-        airlinePartnersPage.getAirPartnersDropDown().click();
         airlinePartnersPage.selectVirginAtlantic();
-        Assert.assertEquals(airlinePartnersPage.getAirPartnersDropDown().getText(), "Virgin Atlantic");
+        Assert.assertEquals(airlinePartnersPage.getVirginAtlanticPageTitle().getText(), "Virgin Atlantic®");
 
     }
-
+    @Test
     public void testWestJet() {
         AirlinePartnersPage airlinePartnersPage = new AirlinePartnersPage();
-        airlinePartnersPage.getAirPartnersDropDown().click();
         airlinePartnersPage.selectWestJet();
-        Assert.assertEquals(airlinePartnersPage.getAirPartnersDropDown().getText(), "WestJet");
+        Assert.assertEquals(airlinePartnersPage.getWestJetPageTitle().getText(), "WestJet");
     }
 }
